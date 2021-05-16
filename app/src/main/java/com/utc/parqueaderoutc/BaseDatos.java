52fe1c3e406ba22a2de1e496b10c8a6bebf14f9c
Angel Tapia <angel.tapia6005@utc.edu.ec>
2021-05-16 16:58:03 -0500

    Inicio ATapia

---
+++ app/src/main/java/com/utc/parqueaderoutc/BaseDatos.java
@@ -0,0 +1,12 @@
+package com.utc.parqueaderoutc;
+
+/*
+ *  @authors: Quishpe Vanesa, Tapia Angel, Vaca Alex A.
+ *  @starts: 16/05/2021
+ *  @last update: 16/05/2021
+ *  @Description: [Es] Base de datos
+ *
+ * */
+
+public class BaseDatos {
+}
