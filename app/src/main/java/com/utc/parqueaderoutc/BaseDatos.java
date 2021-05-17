9042a48a80bab7ece8e0670fd8e5582b74f7c45d
AlexAxl80 <alex.vaca6676@utc.edu.ec>
2021-05-16 19:59:23 -0500

    Disenio Registro AVaca

--- app/src/main/java/com/utc/parqueaderoutc/BaseDatos.java
+++ app/src/main/java/com/utc/parqueaderoutc/BaseDatos.java
@@ -16,45 +16,45 @@ import java.util.Date;
 
 public class BaseDatos extends SQLiteOpenHelper {
 
-    private static final int versionBdd_usuarios = 1;// version de base de Datos
-    private static final String bdd_usuarios = "app_parqueadero";// Nombre de Base de Datos
-    private static final String tablaUsuarios = "create table usuario(" +
-            "id_usu integer primary key autoincrement," +
+    private static final int versionBddParqueadero = 1;// version de base de Datos
+    private static final String nombreBddParqueadero = "app_parqueadero";// Nombre de Base de Datos
+
+    private static final String tablaUsuarios = "CREATE TABLE usuario(" +
+            "id_usu integer PRIMARY KEY AUTOINCREMENT," +
             "apellido_usu text," +
             "nombre_usu text," +
             "email_usu text," +
             "password_usu text," +
-            "telefono_usu text,"+
-            "direccion_usu text,"+
+            "telefono_usu text," +
+            "direccion_usu text," +
             "fecha_registro_usu date)";
 
     //Constructor
-    public BaseDatos(Context contexto){
-        super(contexto,bdd_usuarios,null,versionBdd_usuarios);
+    public BaseDatos(Context contexto) {
+        super(contexto, nombreBddParqueadero, null, versionBddParqueadero);
     }
 
     // Proceso 1
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(tablaUsuarios); // Ejecutar una solicitud SQL, en este caso la creacion de la tabla
-
     }
+
     // Proceso 2
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS usuario"); // Elimina la antigua version de la base de datos
         db.execSQL(tablaUsuarios);
-
     }
 
     // Metodo insercion de datos, retorna TRUE si agrego y FALSE si fallo
-    public boolean agregarUsuarios(String apellido, String nombre, String email, String password, String telefono, String direccion, Date fecha){
+    public boolean agregarUsuarios(String apellido, String nombre, String email, String password, String telefono, String direccion, Date fecha) {
         SQLiteDatabase miBdd = getWritableDatabase(); // Llama la base de datos en el objeto miBdd
 
-        if(miBdd!= null){ // Validar que la base de datos exista
+        if (miBdd != null) { // Validar que la base de datos exista
             // Preparar SQL
             String sql = "INSERT INTO usuario (apellido_usu, nombre_usu, email_usu, password_usu, telefono_usu, direccion_usu, fecha_registro_usu) " +
-                    "VALUES ('"+ apellido +"','"+ nombre +"','"+ email +"','"+ password +"', '"+ telefono +"','"+ direccion +"','"+ fecha +"')";
+                    "VALUES ('" + apellido + "','" + nombre + "','" + email + "','" + password + "', '" + telefono + "','" + direccion + "','" + fecha + "')";
             // Ejecutar el SQL
             miBdd.execSQL(sql);
             // cerrar la conexion
